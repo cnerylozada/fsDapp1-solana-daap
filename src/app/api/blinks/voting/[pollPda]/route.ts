6c1bf33d72b6b223e61aacd81b0b1ec90f123410
cnerylozada <cnerylozada@gmail.com>
2025-07-05 11:31:23 -0500

    feat: post validation

--- src/app/api/blinks/voting/[pollPda]/route.ts
+++ src/app/api/blinks/voting/[pollPda]/route.ts
@@ -1,11 +1,21 @@
 import { Program } from '@coral-xyz/anchor'
-import { ActionGetResponse, createActionHeaders } from '@solana/actions'
-import { clusterApiUrl, Connection, PublicKey } from '@solana/web3.js'
+import {
+  ActionGetResponse,
+  ActionPostRequest,
+  ActionPostResponse,
+  createActionHeaders,
+  createPostResponse,
+  LinkedAction,
+} from '@solana/actions'
+import { clusterApiUrl, Connection, PublicKey, Transaction } from '@solana/web3.js'
 import VOTING_CONTRACT_IDL from '@/contracts/voting/idl.json'
 import { Voting } from '@/contracts/voting/type'
 import { NextResponse } from 'next/server'
 
 const headers = createActionHeaders()
+
+const connection = new Connection(clusterApiUrl('devnet'))
+const votingProgram: Program<Voting> = new Program(VOTING_CONTRACT_IDL, { connection })
 
 export async function GET(request: Request, { params }: { params: Promise<{ pollPda: string }> }) {
   const { pollPda } = await params
@@ -17,18 +27,82 @@ export async function GET(request: Request, { params }: { params: Promise<{ poll
     return NextResponse.json({ error: 'Invalid account' }, { status: 400, headers })
   }
 
-  const connection = new Connection(clusterApiUrl('devnet'))
+  try {
+    const pollAccount = await votingProgram.account.poll.fetch(pollPdaKey)
+    const candidateAccountList = await votingProgram.account.candidate.all()
+    const candidateNameList = candidateAccountList.map((_) => _.account.name)
 
-  const votingProgram: Program<Voting> = new Program(VOTING_CONTRACT_IDL, { connection })
+    const actions: LinkedAction[] = candidateNameList.map((_) => {
+      const requestUrl = new URL(request.url)
+      const href = new URL(`/api/blinks/voting/${pollPda}?candidate=${_}`, requestUrl.origin).toString()
+      return {
+        label: `Vote for ${_}`,
+        href,
+        type: 'transaction',
+      }
+    })
 
-  const pollAccount = await votingProgram.account.poll.fetch(pollPdaKey)
-
-  const payload: ActionGetResponse = {
-    type: 'action',
-    title: pollAccount.poll,
-    icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToIG8feZdv7SVG0RYdGFUy8FDf_wxddAruJQ&s',
-    description: pollAccount.description,
-    label: 'My label',
+    const payload: ActionGetResponse = {
+      title: pollAccount.poll,
+      icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToIG8feZdv7SVG0RYdGFUy8FDf_wxddAruJQ&s',
+      description: pollAccount.description,
+      label: 'Vote!',
+      links: { actions },
+    }
+    return NextResponse.json(payload, { headers })
+  } catch {
+    return NextResponse.json(
+      { error: 'Not found an account associated with given pollPda value' },
+      { status: 404, headers },
+    )
   }
-  return NextResponse.json(payload, { headers })
+}
+
+export const OPTIONS = async () => Response.json(null, { headers })
+
+export async function POST(request: Request) {
+  const body: ActionPostRequest = await request.json()
+  const url = new URL(request.url)
+  const candidate = url.searchParams.get('candidate')
+
+  const candidateAccountList = await votingProgram.account.candidate.all()
+  const candidateNameList = candidateAccountList.map((_) => _.account.name)
+  if (!candidate || !candidateNameList.includes(candidate))
+    return NextResponse.json({ error: `Invalid candidate value` }, { status: 400, headers })
+
+  let voter: PublicKey
+  try {
+    voter = new PublicKey(body.account)
+  } catch {
+    return NextResponse.json({ error: 'Invalid account' }, { status: 400, headers })
+  }
+
+  try {
+    const transferSolInstruction = await votingProgram.methods.vote(candidate).accounts({ signer: voter }).instruction()
+    const blockhash = await connection.getLatestBlockhash()
+    const transaction = new Transaction({
+      feePayer: voter,
+      blockhash: blockhash.blockhash,
+      lastValidBlockHeight: blockhash.lastValidBlockHeight,
+    }).add(transferSolInstruction)
+
+    const payload: ActionPostResponse = await createPostResponse({
+      fields: {
+        transaction,
+        message: `New vote for ${candidate}`,
+        type: 'transaction',
+      },
+    })
+
+    return NextResponse.json(payload, { headers })
+  } catch (error) {
+    console.log(`error`, error)
+    return NextResponse.json(
+      { error: 'An unknown error occurred' },
+      {
+        status: 400,
+        headers,
+      },
+    )
+  }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,18 @@
-import { clusterApiUrl, Connection, Keypair, PublicKey } from '@solana/web3.js'
-import { Program } from '@coral-xyz/anchor'
-import VOTING_CONTRACT from '@/contracts/voting/idl.json'
-import { Voting } from '@/contracts/voting/type'
+import { clusterApiUrl, Connection, PublicKey } from '@solana/web3.js'
 import { DashboardFeature } from '@/components/dashboard/dashboard-feature'
 
 export default async function Home() {
-  const keypair = Keypair.generate()
-
   const connection = new Connection(clusterApiUrl('devnet'))
-  const playgroundWalletKey = new PublicKey('6yBA1xQPxQN7TT5eTMRYgk6zvJm8e9V5VrxcrqisJ7V8')
 
   const votingContractKey = new PublicKey('7PEQLXDuMP9Gbat1CFXJEgUSeaYtchLPP7sKkKFqMvUG')
 
   const info = await connection.getAccountInfo(votingContractKey)
   console.log(`info`, info)
 
-  const votingProgram: Program<Voting> = new Program(VOTING_CONTRACT, { connection })
+  // const poll_account = await votingProgram.account.poll.fetch(pda)
+  // const candidate_pda_list = poll_account.candidatePdaList
 
-  const [pda] = PublicKey.findProgramAddressSync(
-    [Buffer.from('poll_account'), playgroundWalletKey.toBuffer(), Buffer.from('votacion 12:40')],
-    votingContractKey,
-  )
-  const poll_account = await votingProgram.account.poll.fetch(pda)
-  const candidate_pda_list = poll_account.candidatePdaList
-
-  const candidate_list = await votingProgram.account.candidate.fetchMultiple(candidate_pda_list)
+  // const candidate_list = await votingProgram.account.candidate.fetchMultiple(candidate_pda_list)
   // console.log('candidate_list', candidate_list)
 
   return <DashboardFeature />
